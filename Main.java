67ae82bf747d648f52a571266a64d3496f8e982b
piecucci <piecucci@gmail.com>
2024-04-17 23:17:20 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+// DJW-InherAnimal/Main.java
+ 
+public class Main {
+    public static void main (String[] args) {
+        Animal[] animals = {
+                new Dog("Max", 15), new Cat("Batty", 3.5),
+                new Dog("Ajax", 5), new Cat("Minnie", 4)
+        };
+        Animal.voices(animals);
+    }
+}
